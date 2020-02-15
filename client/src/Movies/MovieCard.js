efd9f4212e32ee6bdb7d1f4faa99db2d4dd668fd
Jason <Jason@MacBook-Pro-3.local>
2020-02-15 14:05:29 -0800

    MVP and some stretch

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'
 
-const MovieCard = props => {
-  return;
-};
+const MovieCard = ({ movie }) => {
+  console.log('movie', movie)
+  // console.log('movies', movies)
 
+  // const { title, director, metascore, stars } = movie;
+  return (
+      <NavLink to={`/movies/${movie.id}`} >
+        
+        <div className="movie-card">
+        <h2>{movie.title}</h2>
+        <div className="movie-director">
+          Director: <em>{movie.director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{movie.metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {movie.stars.map(star => (
+          <div key={movie.star} className="movie-star">
+            {star}
+          </div>
+        ))}
+        </div>
+        
+      </NavLink> 
+  );
+}
 export default MovieCard;
