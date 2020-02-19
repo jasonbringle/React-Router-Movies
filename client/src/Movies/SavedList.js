0cfc746b6fb0e8b65833578930a2b43b9c50345e
Jason <Jason@MacBook-Pro-3.local>
2020-02-18 23:45:25 -0800

    Fixed index error in map to include a index

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -4,9 +4,9 @@ import { NavLink } from 'react-router-dom'
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
-    ))}
+    {props.list.map((movie, index)=> {
+      return <span key={index} className="saved-movie">{movie.title}</span>
+    })}
     <NavLink to={"/"}>
     <div className="home-button">Home</div>
     </NavLink>
